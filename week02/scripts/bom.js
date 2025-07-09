a5adfd1c507238dc8b11c560be2e0fc278836b04
xandrineshu <jelina.kawaii@gmail.com>
2025-07-09 23:06:29 +0800

    bom activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value('');
+    }
+    input.focus();
+});
