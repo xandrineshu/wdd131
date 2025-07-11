02db6f3dfa6da0f3de12c21cf46e53e45709c1d6
xandrineshu <jelina.kawaii@gmail.com>
2025-07-11 12:30:23 +0800

    responsive hamburger menu example

---
+++ week02/scripts/hamburger-menu.js
@@ -0,0 +1,11 @@
+// Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+
