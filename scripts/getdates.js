d563bb341dcf2ad52984090f4e01b68ac24df67f
xandrineshu <jelina.kawaii@gmail.com>
2025-07-12 14:14:55 +0800

    js fix

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,5 +2,6 @@ const currentYear = new Date().getFullYear();
 const lastModified = document.lastModified;
 const copyrightYearElement = document.getElementById('currentyear');
 const lastModifiedElement = document.getElementById('lastModified');
+
 copyrightYearElement.textContent = currentYear;
 lastModifiedElement.textContent = `⋆౨ৎ˚⟡ Last update: ${lastModified}`;
