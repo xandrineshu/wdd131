64f1aca06297a5c15bee7454a891bfd4af72bfb4
xandrineshu <jelina.kawaii@gmail.com>
2025-07-24 18:36:08 +0800

    added more temples

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -14,21 +14,74 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const temples = [
     {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+
+    {
         templeName: "Manila Philippines",
         location: "Manila, Philippines",
         dedicated: "1984, September, 25",
         area: 26683,
         imageUrl:
             "https://churchofjesuschristtemples.org/assets/img/temples/_temp/029-Manila-Philippines-Temple.jpg"
-    },
-
-    {
-        templeName: "Washington D.C.",
-        location: "Maryland, US",
-        dedicated: "1974, November, 19",
-        area: 156558,
-        imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
     },
 
     {
@@ -41,39 +94,12 @@ const temples = [
     },
 
     {
-        templeName: "Colonia Juárez Chihuahua Mexico",
-        location: "Chihuahua, Mexico",
-        dedicated: "1999, March, 6",
-        area: 6800,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/colonia-juarez-chihuahua-mexico-temple/colonia-juarez-chihuahua-mexico-temple-1601-main.jpg"
-    },
-
-    {
         templeName: "Bern Switzerland",
         location: " Münchenbuchsee, Switzerland",
         dedicated: "1955, September, 11",
         area: 35546,
         imageUrl:
             "https://churchofjesuschristtemples.org/assets/img/temples/bern-switzerland-temple/bern-switzerland-temple-54641-main.jpg"
-    },
-
-    {
-        templeName: "Brisbane Australia",
-        location: "Queensland, Australia",
-        dedicated: "2003, June, 15",
-        area: 10700,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/brisbane-australia-temple/brisbane-australia-temple-62132-main.jpg"
-    },
-
-    {
-        templeName: "St. George Utah",
-        location: "St. George, Utah",
-        dedicated: "1877, April, 6",
-        area: 143969,
-        imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/st.-george-utah-temple/st.-george-utah-temple-40435-main.jpg"
     },
 
     {
